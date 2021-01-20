1506b0d98119e67441bf7c41ab046189cd1e0312
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2021-01-20 18:33:04 +0300

    Add files via upload

---
+++ probl_zema.py
@@ -0,0 +1,16 @@
+l=[1,5,-2,5,8,3,8,12,-45]
+print(l)
+print(sorted(l))
+y=(sorted(l))
+y.reverse()
+print(y)
+print(len(l))
+print(max(l))
+print(min(l))
+y=l
+y.append(111)
+print(y)
+y=l
+y.insert(1,222)
+print(y)
+
